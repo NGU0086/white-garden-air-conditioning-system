bb714e7c3ec05a60b345cd24da3166e608ba6497
NGU0086 <75478079+NGU0086@users.noreply.github.com>
2021-06-24 21:27:58 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -21,6 +21,7 @@ basic.showLeds(`
 basic.pause(2000)
 basic.showString(" ")
 basic.showString("BEES-TEMP")
+radio.setGroup(88)
 basic.forever(function () {
     temp = input.temperature()
     basic.showNumber(input.temperature())
@@ -29,13 +30,15 @@ basic.forever(function () {
         pins.digitalWritePin(DigitalPin.P0, 1)
         pins.servoWritePin(AnalogPin.P0, 90)
         basic.showString("WARNG HIGH")
+        radio.sendString("TEMPERATURE VERY HIGH - WARNING ISSUED")
     }
     if (input.temperature() <= 25) {
         pins.servoWritePin(AnalogPin.P0, 180)
-        basic.showString("STABLE")
+        basic.showString("STB")
     }
     if (input.temperature() <= 20) {
         pins.servoWritePin(AnalogPin.P0, 90)
         basic.showString("WARNG LOW")
+        radio.sendString("TEMPERATURE VERY LOW - WARNING ISSUED")
     }
 })
