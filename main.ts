d93aab8ac2a385ae963c8bc8b44a9fff0a57b5dc
NGU0086 <75478079+NGU0086@users.noreply.github.com>
2021-06-25 13:09:01 +1000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,17 @@
 input.onButtonPressed(Button.A, function () {
-    pins.servoWritePin(AnalogPin.P0, 180)
+    pins.servoWritePin(AnalogPin.P2, 180)
     basic.showString("OV")
 })
 input.onButtonPressed(Button.B, function () {
-    pins.servoWritePin(AnalogPin.P0, 90)
+    pins.servoWritePin(AnalogPin.P2, 90)
     basic.showString("OV")
 })
 input.onPinPressed(TouchPin.P1, function () {
-    pins.servoWritePin(AnalogPin.P0, 0)
+    pins.servoWritePin(AnalogPin.P2, 0)
     basic.showString("OV")
 })
 let temp = 0
+OLED.init(128, 64)
 basic.showLeds(`
     # # # # #
     # . # . #
@@ -27,17 +28,17 @@ basic.forever(function () {
     basic.showNumber(input.temperature())
     basic.showString("*C")
     if (input.temperature() >= 35) {
-        pins.digitalWritePin(DigitalPin.P0, 1)
-        pins.servoWritePin(AnalogPin.P0, 90)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        pins.servoWritePin(AnalogPin.P2, 90)
         basic.showString("WARNG HIGH")
         radio.sendString("TEMPERATURE VERY HIGH - WARNING ISSUED")
     }
     if (input.temperature() <= 25) {
-        pins.servoWritePin(AnalogPin.P0, 180)
+        pins.servoWritePin(AnalogPin.P2, 180)
         basic.showString("STB")
     }
     if (input.temperature() <= 20) {
-        pins.servoWritePin(AnalogPin.P0, 90)
+        pins.servoWritePin(AnalogPin.P2, 90)
         basic.showString("WARNG LOW")
         radio.sendString("TEMPERATURE VERY LOW - WARNING ISSUED")
     }
